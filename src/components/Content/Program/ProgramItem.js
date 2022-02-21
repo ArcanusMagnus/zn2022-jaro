3d479a5cc07d5f7b5007f0677e9ba2641a1c8781
Jan Majerníček <arcanusvontenebris@gmail.com>
2022-02-21 21:53:55 +0100

    Improve and refactor program into smaller components

---
+++ src/components/Content/Program/ProgramItem.js
@@ -0,0 +1,16 @@
+import styles from "./ProgramItem.module.css";
+
+const ProgramItem = (props) => {
+  return (
+    <li className={styles.programItem}>
+      <span>
+        {props.show.StartTime} 
+      </span>
+      <span>
+        <a href={props.show.Link}>{props.show.Name}</a> <span>({props.show.Genre})</span>
+      </span>
+    </li>
+  );
+};
+
+export default ProgramItem;
